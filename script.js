afa313811c99995b0aba6dc52d9f761b1ca6b7d7
Sohail <shaikhsohail1822@gmail.com>
2022-08-07 21:59:18 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+AOS.init({
+    offset:200,
+    delay: 200,
+    duration: 1000,
+    easing:"ease-in-out",
+    once:"false",
+    mirror :"false",
+    anchorplacement :"top-bottom"
+});
+
+
+// const hamburgerMenu = document.querySelector(" #navmenu  .navbar")
+// const closeNavButton = document.querySelector("#navmenu  .navbar")
+
+
+// hamburgerMenu.addEventListener("click", ()=>{
+//     closeNavButton.classList.add("show")
+//     document.body.style.overflow = 'hidden'
+// })
+// closeNavButton.addEventListener("click" , ()=>{
+//     closeNavButton.classList.remove("show")
+//     document.body.style.overflow = 'initial'
+// })
